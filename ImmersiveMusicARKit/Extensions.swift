4903ffc74a72abbb184ddb492c8fcdf2f07e4387
Dustin Freeman <dustin.freeman@gmail.com>
2017-12-09 14:58:40 -0500

    Start SceneKit Extensions

---
+++ ImmersiveMusicARKit/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  ImmersiveMusicARKit
+//
+//  Created by Dustin Freeman on 2017-12-09.
+//  Copyright © 2017 Dustin Freeman. All rights reserved.
+//
+
+import SceneKit
+
+extension SCNVector3 {
+    static func -(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+        return SCNVector3(left.x - right.x, left.y - right.y, left.z - right.z)
+    }
+}
